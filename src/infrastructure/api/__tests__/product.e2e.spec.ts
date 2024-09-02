63e2dcfa2a2452261657b46c89f4049e837f4d55
Wagner <wagnerpalmeira.ti@gmail.com>
2024-09-02 16:55:14 -0300

    Adding List Products endpoint

---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,26 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for product", () => {
+    beforeEach(async () => {
+        await sequelize.sync({ force: true });
+    });
+
+    afterAll(async () => {
+        await sequelize.close();
+    });
+
+
+  it("should create a product", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Product 1",
+        price: 199,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("Product 1");
+    expect(response.body.price).toBe(199);
+  });
+})
